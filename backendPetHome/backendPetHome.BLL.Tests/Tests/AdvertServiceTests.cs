6b8af17046b5050648d1863eb4e2dde86279c379
Danil <danilsmy.edu@gmail.com>
2022-12-31 09:25:09 +0200

    Add UserService unit tests with mocks

---
+++ backendPetHome/backendPetHome.BLL.Tests/Tests/AdvertServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using backendPetHome.BLL.Services;
+using backendPetHome.DAL.Interfaces;
+using Moq;
+
+namespace backendPetHome.BLL.Tests.Tests
+{
+    public class AdvertServiceTests
+    {
+        private readonly AdvertService _sut;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock = new Mock<IUnitOfWork>();
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+        public AdvertServiceTests()
+        {
+            //_sut = new AdvertService(_unitOfWorkMock.Object,);
+        }
+    }
+}
