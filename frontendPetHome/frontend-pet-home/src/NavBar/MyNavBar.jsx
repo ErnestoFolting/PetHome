0d2ab7a3eb08f00da6ba885e9b63414ced6848c7
Danil <danilsmy.edu@gmail.com>
2022-12-31 02:04:21 +0200

    Add responsive burger menu

--- frontendPetHome/frontend-pet-home/src/NavBar/MyNavBar.jsx
+++ frontendPetHome/frontend-pet-home/src/NavBar/MyNavBar.jsx
@@ -1,11 +1,14 @@
-import { React, useContext } from 'react'
+import { React, useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MyButton } from '../UI/buttons/MyButton'
 import { Context } from '../index'
 import { observer } from 'mobx-react-lite'
 import s from './MyNavBar.module.css'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import Logo from '../Assets/PetHome.png'
 
 const MyNavBar = () => {
+  const [nav, setNav] = useState(false);
   const { store } = useContext(Context);
   if (!store.isAuth) {
     return (
@@ -16,15 +19,21 @@ const MyNavBar = () => {
   }
   return (
     <div className={s.navBar}>
+
+
+      <Link to="/adverts" onClick={() => setNav(!nav)}><img src={Logo} alt='logo' href='/adverts' /></Link>
+      <div className={nav ? [s.menu, s.active].join(' ') : [s.menu]}>
+        <Link to="/adverts" onClick={() => setNav(!nav)}>Оголошення</Link>
+        <Link to="/create" onClick={() => setNav(!nav)}>Створити оголошення</Link>
+        <Link to="/myadverts" onClick={() => setNav(!nav)}>Мої оголошення</Link>
+        <Link to="/myrequests" onClick={() => setNav(!nav)}>Мої заявки</Link>
+        <Link to="/myprofile" onClick={() => setNav(!nav)}>Профіль</Link>
+      </div>
+      <div onClick={() => setNav(!nav)} className={s.mobile_btn}>
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+      </div>
       <div>
         {store.isAuth && <MyButton onClick={store.logout} style={{ boxShadow: 'none' }}>Вийти</MyButton>}
-      </div>
-      <div className={s.navBar__links}>
-        <Link to="/adverts">Оголошення</Link>
-        <Link to="/create">Створити оголошення</Link>
-        <Link to="/myadverts">Мої оголошення</Link>
-        <Link to="/myrequests">Мої заявки</Link>
-        <Link to="/myprofile">Профіль</Link>
       </div>
     </div>
   )
